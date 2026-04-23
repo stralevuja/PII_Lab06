ffb8333c641c0876f96d092fad53b9d105b2b42f
Strahinja Vujasin <73841386+stralevuja@users.noreply.github.com>
2026-04-23 12:33:00 +0200

    Redundancy fixed in add_begin

--- L06_01/main.c
+++ L06_01/main.c
@@ -13,28 +13,28 @@ typedef struct cvor {
     struct cvor *next;
 } CVOR;
 
-CVOR* add_begin(CVOR **glava, STUDENT *s);
+void add_begin(CVOR **glava, STUDENT *s);
 void printflista(CVOR *glava);
 void fprintflista(CVOR *glava, const char *filename);
 
 int main(void) {
     CVOR *glava = NULL;
-
+    
     STUDENT s1 = {"Marko", "Markovic", "12345"};
     STUDENT s2 = {"Janko", "Jankovic", "54321"};
     STUDENT s3 = {"Ana", "Anic", "67890"};
-
-    glava = add_begin(&glava, &s1);
-    glava = add_begin(&glava, &s2);
-    glava = add_begin(&glava, &s3);
-
+    
+    add_begin(&glava, &s1);
+    add_begin(&glava, &s2);
+    add_begin(&glava, &s3);
+    
     printf("Lista studenata:\n");
     printflista(glava);
-
+    
     fprintflista(glava, "lista.txt");
     printf("\n");
     printf("Lista studenata upisana u fajl.\n");
-
+    
     CVOR *tmp = glava;
     while(tmp != NULL) {
         CVOR *sledeci = tmp->next;
@@ -42,11 +42,10 @@ int main(void) {
         tmp = sledeci;
     }
     glava = NULL;
-
     return 0;
 }
 
-CVOR* add_begin(CVOR **glava, STUDENT *s) {
+void add_begin(CVOR **glava, STUDENT *s) {
     CVOR *novi = (CVOR*)malloc(sizeof(CVOR));
     if (novi == NULL) {
         printf("Greska pri alokaciji memorije!\n");
@@ -55,7 +54,6 @@ CVOR* add_begin(CVOR **glava, STUDENT *s) {
     novi->s = *s;
     novi->next = *glava;
     *glava = novi;
-    return novi;
 }
 
 void printflista(CVOR *glava) {
